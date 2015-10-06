0bb2c80614c8391021847dbf278fc6945aaefe28
Paul Doyle <pauleamondoyle@gmail.com>
2015-10-06 00:49:25 -0500

    first draft

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -3,6 +3,9 @@ require("babel/polyfill")
 
 let fetch = require('./fetcher')
 
+var React = require('react')
+
+
 // other stuff that we don't really use in our own code
 // var Pace = require("../bower_components/pace/pace.js")
 
@@ -16,3 +19,77 @@ let fetch = require('./fetcher')
     // new Router()
 // }
 
+// ===============================================
+
+var ClockMachine = React.createClass({
+
+	getInitialState: function(){
+		return{
+			year: 2015,
+			ticking: false
+		}
+	},
+
+
+	tickForward: function(){
+		var self = this
+		if (!this.state.ticking){
+			self.setState({
+				year: self.state.year + 1,
+				ticking: true
+			})
+			setInterval(function(){
+				self.setState({
+					year: self.state.year + 1,
+					ticking: true
+				})
+			}, 1000)
+		}
+		else {
+			clearInterval(this.tickForward)
+		}
+	},
+
+
+	tickBackward: function(){
+		var self = this
+		if (!this.state.ticking){
+			self.setState({
+				year: self.state.year - 1,
+				ticking: true
+			})
+			setInterval(function(){
+				self.setState({
+					year: self.state.year - 1,
+					ticking: true
+				})
+			}, 1000)
+		}
+	},
+
+	tickPause: function(){
+		// var self = this
+		// if (this.state.ticking){
+		// 	self.setState({year: self.state.year,
+		// 					ticking: false}) 
+		// 	// setTimeout(this.tickPause, 1000)
+		// }
+		
+		clearInterval(this.tickForward)
+	},
+
+	render: function(){
+	return(
+		<div>
+			<p className='readout'> {this.state.year} </p>
+			<button onClick={this.tickBackward} className='tickButton'>&#60;</button>
+			<button onClick={this.tickPause} className='tickButton'>||</button>
+			<button onClick={this.tickForward} className='tickButton'>&#62;</button>
+		</div>
+
+	)}
+
+
+})
+
+React.render(<ClockMachine/>,document.getElementById('clockContainer'))
